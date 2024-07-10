c1249cbf7a2ae2d42ee31f5be525509414d8342c
shan wolff <shanwolff182@gmail.com>
2024-07-10 12:38:07 -0400

    added code, removed php

---
+++ forms/contact.js
@@ -0,0 +1,19 @@
+
+// // JavaScript code to handle form submission
+// const form = document.getElementById('contactForm');
+// form.addEventListener('submit', function (event) {
+//     event.preventDefault(); // prevent form submission
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     const message = document.getElementById('message').value;
+//     const data = {
+//         name,
+//         email,
+//         message
+//     };
+//     // Send data to AWS Amplify Lambda function
+//     // Here you can use AWS SDK or any other method to send the data to your Lambda function
+//     console.log(data);
+//     // Clear form fields after submission
+//     form.reset();
+// });
